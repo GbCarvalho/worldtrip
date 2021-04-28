a9ed8d96fbf24d53b279bcf5de106d71f1bb712b
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2021-04-27 23:10:12 -0300

    Adjusted some components at the Europe page and adjusted the Header component to recieve a throw back function

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
 import { Image } from "@chakra-ui/image";
 import { Flex } from "@chakra-ui/layout";
 
-export function Header() {
+interface HeaderProps {
+  throwBack?: string;
+}
+
+export function Header({ throwBack = null }: HeaderProps) {
   return (
     <Flex
       as="header"
@@ -9,6 +13,8 @@ export function Header() {
       alignItems="center"
       w="100%"
       h="28"
+      maxW="1440px"
+      m="auto"
     >
       <Image src="/images/logo.png" />
     </Flex>
--- src/pages/europe.tsx
+++ src/pages/europe.tsx
@@ -1,5 +1,7 @@
+import Icon from "@chakra-ui/icon";
 import { Image } from "@chakra-ui/image";
-import { Flex, HStack, Stack, Text, VStack } from "@chakra-ui/layout";
+import { Flex, Grid, HStack, Text, VStack } from "@chakra-ui/layout";
+import { Tooltip } from "@chakra-ui/tooltip";
 import { CityCard } from "../components/CityCard";
 import { Header } from "../components/Header";
 
@@ -14,7 +16,12 @@ export default function Europe() {
         <Flex maxW={1160} mx="auto" flexDir="column">
           {/* Continent description */}
 
-          <Flex flex="1" mt="20">
+          <Flex
+            flex="1"
+            mt="20"
+            justifyContent="space-between"
+            alignItems="center"
+          >
             <Text w="50%" textAlign="justify" lineHeight="36px">
               A Europa é, por convenção, um dos seis continentes do mundo.
               Compreendendo a península ocidental da Eurásia, a Europa
@@ -22,6 +29,8 @@ export default function Europe() {
               montes Urais, o rio Ural, o mar Cáspio, o Cáucaso, e o mar Negro a
               sudeste
             </Text>
+
+            {/* Data details from the continent */}
             <Flex w="50%" justifyContent="space-evenly" alignItems="center">
               <Flex flexDir="column" textAlign="center">
                 <Text fontSize="48" color="yellow.500" fontWeight="600">
@@ -43,9 +52,14 @@ export default function Europe() {
                 <Text fontSize="48" color="yellow.500" fontWeight="600">
                   27
                 </Text>
-                <Text fontSize="24" color="gray.700" fontWeight="600">
-                  cidades +100
-                </Text>
+                <Flex flexDir="row">
+                  <Text fontSize="24" color="gray.700" fontWeight="600" mr="2">
+                    cidades +100
+                  </Text>
+                  <Tooltip label="top 100 cidades mais visitadas no mundo">
+                    <Image src="/images/info.svg" />
+                  </Tooltip>
+                </Flex>
               </Flex>
             </Flex>
           </Flex>
@@ -59,19 +73,38 @@ export default function Europe() {
               Cidades +100
             </Text>
 
-            {/* This might be an temporary solution */}
-
-            <VStack spacing="12" mt="10" mb="35">
-              <HStack spacing="45px" justify="start" align="center">
-                <CityCard />
-                <CityCard />
-                <CityCard />
-                <CityCard />
-              </HStack>
-              <HStack spacing="45px" justify="start" w="100%">
-                <CityCard />
-              </HStack>
-            </VStack>
+            <Grid templateColumns="repeat(4, 1fr)" gap={12} mt="10" mb="35">
+              <CityCard
+                flag="https://cdn.countryflags.com/thumbs/united-kingdom/flag-round-250.png"
+                photo="/images/country_image.png"
+                city="Londres"
+                country="Reino Unido"
+              />
+              <CityCard
+                photo="https://images.unsplash.com/photo-1431274172761-fca41d930114?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                flag="https://cdn.countryflags.com/thumbs/france/flag-round-250.png"
+                city="Paris"
+                country="França"
+              />
+              <CityCard
+                photo="https://images.unsplash.com/photo-1597177724434-245eb4d730e7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1268&q=80"
+                flag="https://cdn.countryflags.com/thumbs/italy/flag-round-250.png"
+                city="Roma"
+                country="Itália"
+              />
+              <CityCard
+                photo="https://images.unsplash.com/photo-1562624475-96c2bc08fab9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+                flag="https://cdn.countryflags.com/thumbs/czech-republic/flag-round-250.png"
+                city="Praga"
+                country="República Tcheca"
+              />
+              <CityCard
+                photo="https://images.unsplash.com/photo-1512470876302-972faa2aa9a4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                flag="https://cdn.countryflags.com/thumbs/netherlands/flag-round-250.png"
+                city="Amstedã"
+                country="Holanda"
+              />
+            </Grid>
           </Flex>
         </Flex>
       </Flex>
