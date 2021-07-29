2441fd345b2c0f23ec74c5730c04076ee88a89e2
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2021-07-29 11:36:53 -0300

    Header routing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
 import { Image } from "@chakra-ui/image";
 import { Flex } from "@chakra-ui/layout";
+import { useRouter } from "next/router";
 
 interface HeaderProps {
   throwBack?: string;
 }
 
 export function Header({ throwBack = null }: HeaderProps) {
+  const router = useRouter();
+
   return (
     <Flex
       as="header"
@@ -15,6 +18,8 @@ export function Header({ throwBack = null }: HeaderProps) {
       h="28"
       maxW="1440px"
       m="auto"
+      cursor="pointer"
+      onClick={() => router.push("/")}
     >
       <Image src="/images/logo.png" />
     </Flex>
